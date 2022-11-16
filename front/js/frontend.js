8bebf35462bd85dc1d618beb32bae968532efae4
Rodrigo Bossini <professorbossini@gmail.com>
2022-11-16 07:53:53 -0300

    retomar do bloco de codigo 2.13.2, fazendo a funcao cadastrarFilme

--- front/js/frontend.js
+++ front/js/frontend.js
@@ -7,7 +7,15 @@ async function obterFilmes(){
   //datum: dado
   //data: dados
   const filmes = (await axios.get(URLCompleta)).data
-  console.log(filmes)
+  const tabela = document.querySelector('.filmes')
+  const corpoTabela = tabela.getElementsByTagName('tbody')[0]
+  for (let filme of filmes){
+    const linha = corpoTabela.insertRow(0)
+    const celulaTitulo = linha.insertCell(0)
+    const celulaSinopse = linha.insertCell(1)
+    celulaTitulo.innerHTML = filme.titulo
+    celulaSinopse.innerHTML = filme.sinopse
+  }
 }
 
 //CORS: Cross Origin Resource Sharing
